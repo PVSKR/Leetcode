9ff64c25f2346c38d0c90cb81e044aab0da91512
PVSKR <63577468+PVSKR@users.noreply.github.com>
2024-07-08 16:25:41 -0700

    Time: 0 ms (100%), Space: 42.9 MB (7.67%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,17 +10,26 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null) return null;
-        ListNode current = head;
-        ListNode prev = null;
-        ListNode front = null;
+        // if(head == null) return null;
+        // ListNode current = head;
+        // ListNode prev = null;
+        // ListNode front = null;
 
-        while(current != null) {
-            front = current.next;
-            current.next = prev;
-            prev = current;
-            current = front;
-        }
-        return prev; 
+        // while(current != null) {
+        //     front = current.next;
+        //     current.next = prev;
+        //     prev = current;
+        //     current = front;
+        // }
+        // return prev; 
+
+        return rev(head, null);
     }
+
+    public ListNode rev(ListNode node, ListNode prev) {
+        if(node == null) return prev;
+        ListNode temp = node.next;
+        node.next = prev;
+        return rev(temp, node);
+    } 
 }
