5c9895a1d69cc7402aa222a72c1f8240592b541c
PVSKR <63577468+PVSKR@users.noreply.github.com>
2024-07-28 17:16:31 -0700

    Time: 0 ms (100%), Space: 41.9 MB (73.08%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,23 +14,35 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root==null) return ans;
-        bfs(ans, root);
+    // public List<Integer> rightSideView(TreeNode root) {
+    //     List<Integer> ans = new ArrayList<>();
+    //     if(root==null) return ans;
+    //     bfs(ans, root);
+    //     return ans;
+    // }
+    // private void bfs(List<Integer> ans, TreeNode root) {
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.offer(root);
+    //     while(!q.isEmpty()) {
+    //         int level = q.size();
+    //         for(int i=0; i<level; i++) {
+    //             TreeNode curr = q.poll();
+    //             if(i==0) ans.add(curr.val);
+    //             if(curr.right!=null) q.offer(curr.right);
+    //             if(curr.left!=null) q.offer(curr.left);
+    //         }
+    //     }
+    // }
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {        
+        rightView(root, 0);
         return ans;
     }
-    private void bfs(List<Integer> ans, TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()) {
-            int level = q.size();
-            for(int i=0; i<level; i++) {
-                TreeNode curr = q.poll();
-                if(i==0) ans.add(curr.val);
-                if(curr.right!=null) q.offer(curr.right);
-                if(curr.left!=null) q.offer(curr.left);
-            }
-        }
+    private void rightView(TreeNode root, int level) {
+        if(root==null) return;
+        if(level == ans.size()) ans.add(root.val);
+
+        rightView(root.right, level+1);
+        rightView(root.left, level+1);
     }
 }
