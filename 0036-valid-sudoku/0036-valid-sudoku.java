f8e8197a1d5ac925e59d27af096173fe1aa5dd78
PVSKR <63577468+PVSKR@users.noreply.github.com>
2025-01-22 01:50:10 -0700

    Time: 14 ms (18.02%), Space: 44.8 MB (41.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,21 +1,34 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] row = new boolean[9][9];
-        boolean[][] col = new boolean[9][9];
-        boolean[][] subGrid = new boolean[9][9];
+        // boolean[][] row = new boolean[9][9];
+        // boolean[][] col = new boolean[9][9];
+        // boolean[][] subGrid = new boolean[9][9];
         
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] == '.') continue;
-                int c = board[i][j] - '1';
-                int subi = i/3, subj = j/3;
-                int posInSubGrid = subi*3 + subj;
-                if(row[i][c] || col[c][j] || subGrid[posInSubGrid][c]) return false;
-                row[i][c] = true;
-                col[c][j] = true;
-                subGrid[posInSubGrid][c] = true;
+        // for(int i = 0; i < 9; i++) {
+        //     for(int j = 0; j < 9; j++) {
+        //         if(board[i][j] == '.') continue;
+        //         int c = board[i][j] - '1';
+        //         int subi = i/3, subj = j/3;
+        //         int posInSubGrid = subi*3 + subj;
+        //         if(row[i][c] || col[c][j] || subGrid[posInSubGrid][c]) return false;
+        //         row[i][c] = true;
+        //         col[c][j] = true;
+        //         subGrid[posInSubGrid][c] = true;
+        //     }
+        // }
+        // return true;
+
+        Set<String> set = new HashSet<>();
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                char c = board[i][j];
+                if(board[i][j] != '.') {
+                    if(!set.add(c + "in row" + i) ||
+                     !set.add(c + "in col" + j) ||
+                     !set.add(c + "in block" + i/3 + "," + j/3)) return false;
+                }
             }
         }
-        return true;
+        return true; 
     }
 }
