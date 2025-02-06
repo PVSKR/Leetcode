e0c41e7dd85589d7cc5ef692b9d843a10d45e579
PVSKR <63577468+PVSKR@users.noreply.github.com>
2025-02-06 01:38:39 -0700

    Time: 2 ms (75.42%), Space: 62.1 MB (18.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int minPrice = prices[0];
-        for(int i=0; i<prices.length; i++) {
-            minPrice = Math.min(minPrice, prices[i]);
-            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        // int maxProfit = 0;
+        // int minPrice = prices[0];
+        // for(int i=0; i<prices.length; i++) {
+        //     minPrice = Math.min(minPrice, prices[i]);
+        //     maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        // }
+        // return maxProfit;
+        int left=0, right=0, maxProfit=0;
+        while(right<prices.length) {
+            if(prices[left]<prices[right]) maxProfit = Math.max(maxProfit, prices[right]-prices[left]);
+            else left = right;
+            right++;
         }
         return maxProfit;
     }
